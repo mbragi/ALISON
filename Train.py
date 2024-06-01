136c81af7293a6e43f05bdc40a3e75902be762f4
Gar Manji Michael <spontaneousmanji@gmail.com>
2024-06-01 15:56:23 +0000

    Refactor Train.py to fix ngram_rep function call

--- Train.py
+++ Train.py
@@ -97,7 +97,7 @@ def main():
             print(f'{processed} texts processed')
 
         y.append(int(row['label']))
-        X.append(ngram_rep(row['text'], row[2], args.V, n_grams, args.V, pos_n_grams, args.V, word_n_grams))
+        X.append(ngram_rep(row['text'], row[2], [args.V, n_grams, args.V, pos_n_grams, args.V, word_n_grams]))
 
         processed += 1
 
